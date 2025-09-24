bb7bf13d8ca5fafadee9332c79124777996cb2ac
Artiom R <ar@enty.io>
2025-09-24 11:10:16 +0200

    Update site configuration with new domain, author, description, and title.

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://notki.fyi/", // replace this with your deployed domain
+  author: "Ariston Ryżabkou",
+  desc: "Заметки о повседневности и переменах",
+  title: "Нотки на полях",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
