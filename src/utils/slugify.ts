e9c4c892d0bb09f98d43c59e1feb836b41d7317c
Artiom R <ar@enty.io>
2025-09-25 13:29:59 +0200

    Refactor slug generation to use `post.slug` instead of `post.data.title`.

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,5 +1,8 @@
 import { slug as slugger } from "github-slugger";
 
-export const slugifyStr = (str: string) => slugger(str);
+export const slugifyStr = (str: string) => {
+
+  return slugger(str);
+};
 
 export const slugifyAll = (arr: string[]) => arr.map(str => slugifyStr(str));
